9848e63089a7840ed5df66585cb9063ee8a8fe47
Michael Häfner <haefnermichael@gmx.de>
2014-03-02 23:00:17 +0100

    added a grunt copy for deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,19 +46,35 @@ module.exports = function(grunt) {
           'dev/css/normalize.css': 'dev/scss/normalize.scss'
         }
       }
-    }
+    },
 
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'dev/img/', src: ['**'], dest: 'dep/img/'},
+          {expand: true, cwd: 'dev/css/', src: ['**'], dest: 'dep/css/'},
+          {expand: true, cwd: 'dev/fonts/', src: ['**'], dest: 'dep/fonts/'},
+          {src: 'dev/optimized.js', dest:'dep/optimized.js'},
+          {src: 'dev/index.html', dest:'dep/index.html'},
+          {src: 'dev/js/require.js', dest:'dep/js/require.js'},
+          {src: 'dev/js/vendor/custom.modernizr.js', dest:'dep/js/vendor/custom.modernizr.js'}
+        ]
+      }
+    }
   });
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
 
   // Default task(s).
   grunt.registerTask('default', ['requirejs', 'sass']);
   grunt.registerTask('css', ['sass']);
   grunt.registerTask('js', ['requirejs']);
+  grunt.registerTask('cp', ['copy']);
 
 };
